2bd664a39202d4c21a996bc7173a47d065cfcc81
fabriziobertoglio1987 <fabrizio.bertoglio@gmail.com>
2020-06-13 09:59:30 +0200

    speech recognition working example

--- src/components/Microphone.js
+++ src/components/Microphone.js
@@ -1,34 +1,70 @@
+import React from 'react';
+import { StyleSheet, Button } from 'react-native';
 import Voice from '@react-native-community/voice';
-import React, {Component} from 'react';
-import { Permissions, Button } from 'react-native';
 
-class Microphone extends Component {
+export default class Microphone extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      recognized: '',
+      started: '',
+      results: [],
+    };
+    Voice.onSpeechStart = this.onSpeechStart.bind(this);
+    Voice.onSpeechRecognized = this.onSpeechRecognized.bind(this);
+    Voice.onSpeechResults = this.onSpeechResults.bind(this);
   }
 
-  onStartButtonPress = () => {
-    Voice.start('en-US');
+  componentWillUnmount() {
+    Voice.destroy().then(Voice.removeAllListeners);
   }
 
-  async componentDidMount() {
-    const { status, expires, permissions } = await Permissions.askAsync(
-      Permissions.AUDIO_RECORDING
-    );
-    if (status !== "granted") {
-      this.setState({showRecordButton: false});
-    } else {
-      this.setState({showRecordButton: true});
+  onSpeechStart(e) {
+    this.setState({
+      started: '√',
+    });
+  };
+
+  onSpeechRecognized(e) {
+    this.setState({
+      recognized: '√',
+    });
+  };
+
+  onSpeechResults(e) {
+    console.warn('you said', e.value);
+    this.setState({
+      results: e.value,
+    });
+  }
+
+  async _startRecognition(e) {
+    this.setState({
+      recognized: '',
+      started: '',
+      results: [],
+    });
+    try {
+      await Voice.start('en-US');
+    } catch (e) {
+      console.error(e);
     }
   }
 
-  render() {
+  render () {
     return (
-      <Button
-        title="Voice Control"
-        onPress={this.onStartButtonPress} />
-    )
+      <Button style={styles.transcript}
+        onPress={this._startRecognition.bind(this)}
+        title="Start" />
+    );
   }
 }
 
-export default Microphone;
+const styles = StyleSheet.create({
+  transcript: {
+    textAlign: 'center',
+    color: '#B0171F',
+    marginBottom: 1,
+    top: '400%',
+  },
+});
